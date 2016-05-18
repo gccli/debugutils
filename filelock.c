3dad98f4bf6392231b51d7ee061f2ea7614885fc
Jing Li <lijing@example.com>
2016-05-18 16:35:45 +0800

    file lock

--- filelock.c
+++ filelock.c
@@ -17,22 +17,27 @@ int lock_write_pid(const char *lockf, const char *pidfile)
     fd = -1;
     ret = EINVAL;
     do {
-        fd = open(lockf, O_CREAT|O_WRONLY|O_TRUNC, 0600);
+        fd = open(lockf, O_CREAT|O_WRONLY|O_TRUNC, 0644);
         if (fd < 0) {
-            printf("open(%s) error - %s\n", lockf, strerror(errno));
+            ret = errno;
+            fprintf(stderr, "open(%s) error - %s\n", lockf, strerror(errno));
             break;
         }
 
         ret = flock(fd, LOCK_EX|LOCK_NB);
         if (ret < 0) {
-            printf("flock(%s) error - %s\n", lockf, strerror(errno));
+            ret = errno;
+            close(fd);
+            fprintf(stderr, "flock(%s) error - %s\n", lockf, strerror(errno));
+
             break;
         }
 
-        fd = open(pidfile, O_CREAT|O_WRONLY|O_TRUNC);
+        fd = open(pidfile, O_CREAT|O_WRONLY|O_TRUNC, 0644);
         if (fd < 0) {
-            printf("open(%s) error - %s\n", pidfile, strerror(errno));
+            ret = errno;
             close(fd);
+            fprintf(stderr, "open(%s) error - %s\n", pidfile, strerror(errno));
             break;
         }
 
@@ -45,3 +50,17 @@ int lock_write_pid(const char *lockf, const char *pidfile)
 
     return ret;
 }
+
+#ifdef UT_TEST
+int main(int argc, char *argv[])
+{
+    int ret = lock_write_pid("x.lock", "x.pid");
+    if (ret != 0) {
+        printf("Another process is running - %s\n", strerror(errno));
+        return ret;
+    }
+    printf("ok\n");
+    while(1) { sleep(1); }
+    return 0;
+}
+#endif
